aa6f5abf6476765ad9518cf39cd13d655b13a813
Andreas Gagas <tug25511@temple.edu>
2020-07-18 17:57:29 -0400

    component for homepage, browseby, layout complete

---
+++ src/components/BrowseBy.js
@@ -0,0 +1,100 @@
+import React from "react"; 
+import styled from "styled-components"; 
+
+function BrowseBy() {
+  // BrowseBy Popular, Rating, Genre, Year 
+  // path home/popular , home/rating .... 
+
+  // home/popular/this/month ... 
+
+  return (
+    <Container>
+      <Label>BROWSE BY</Label>
+
+      <Container2>
+        <Select>
+          <Option hidden>POPULAR</Option>
+          <Option>ALL TIME</Option>
+          <Option>THIS YEAR</Option>
+          <Option>THIS MONTH</Option>
+          <Option>THIS WEEK</Option>
+        </Select>
+        <Select>
+          <Option hidden>RATING</Option>
+          <Option>HIGH FIRST</Option>
+          <Option>LOW FIRST</Option>
+        </Select>
+        <Select>
+          <Option hidden>GENRE</Option>
+          <Option>ACTION</Option>
+          <Option>ANIMATION</Option>
+          <Option>COMEDY</Option>
+          <Option>CRIME</Option>
+          <Option>DRAMA</Option>
+          <Option>HISTORY</Option>
+          <Option>HORROR</Option>
+          <Option>MYSTERY</Option>
+          <Option>ROMANCE</Option>
+          <Option>SCIENCE FICTION</Option>
+          <Option>THRILLER</Option>
+          <Option>WESTERN</Option>
+          <Option>WAR</Option>
+        </Select>
+        </Container2>
+    </Container>
+  )
+}
+
+// Style 
+const Container = styled.div`
+  font-family: Roboto; 
+  margin-top: 1%; 
+  width: 700px;
+  height: 50px; 
+  position: relative;
+  left: 25%; 
+  display: flex; 
+  align-items: center; 
+
+
+`;
+
+const Container2 = styled.div`
+  position: relative; 
+  left: 2%; 
+  outline: 1px solid #5e6a76; 
+
+  display: flex;
+  align-items: center; 
+`;
+
+const Label = styled.label`
+  color: #e1e3e5;
+  font-size: .9rem; 
+`;
+
+const Select = styled.select`
+  font-family: Roboto; 
+  font-size: 1.0rem; 
+  background-color: #13181c; 
+  color: #e1e3e5;
+  padding: 5px; 
+  width: 140px;
+  text-indent: 10px; 
+`;
+
+const Option = styled.option`
+  background-color: #8699aa; 
+  color: #333; 
+  text-align: center; 
+  font-size: 1.1rem; 
+  
+  
+  
+
+`;
+
+
+
+
+export default BrowseBy; 
