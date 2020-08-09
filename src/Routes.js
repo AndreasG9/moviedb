f4fbe1e9b9a655314244c66f40f48d462d5f98fe
Andreas Gagas <tug25511@temple.edu>
2020-08-08 22:35:48 -0400

    404 for bad path

--- src/Routes.js
+++ src/Routes.js
@@ -1,5 +1,5 @@
 import React from "react";
-import { BrowserRouter as Router, Route} from "react-router-dom"; 
+import { BrowserRouter as Router, Route, Redirect, Switch} from "react-router-dom"; 
 import Home from "./pages/Home";
 import SearchPage from "./pages/SearchPage"; 
 import FilmPage from "./pages/FilmPage"; 
@@ -8,75 +8,82 @@ import PersonPage from "./pages/PersonPage";
 import ProfilePage from "./pages/User/ProfilePage.js";
 import UserFilmsPage from "./pages/User/UserFilmsPage.js";  
 import UserListsPage from "./pages/User/UserListsPage.js";  
+import Error from "./pages/Page404"; 
 
 const Routes = () => (
-  <Router>
+    <Router>
+      <Switch>
+      <Route exact path="/" component={Home}></Route>
+      
+      <Route 
+        path="/search" 
+        render = { (props) => 
+          <SearchPage 
+            query={props.location.state.query}>
+          </SearchPage>}>
+      </Route>
 
-    <Route exact path="/" component={Home}></Route>
-    
-    <Route 
-      path="/search" 
-      render = { (props) => 
-        <SearchPage 
-          query={props.location.state.query}>
-        </SearchPage>}>
-    </Route>
+      <Route
+        path="/film"
+        render = { (props) => 
+          <FilmPage
+          movie_id={props.location.state.movie_id}>
+          </FilmPage>}>
+      </Route>
 
-    <Route
-      path="/film"
-      render = { (props) => 
-        <FilmPage
-         movie_id={props.location.state.movie_id}>
-        </FilmPage>}>
-    </Route>
+      <Route
+        path="/films"
+        render = { (props) => (
+          <FilmsPage
+            browseby={props.location.state.browseby}
+            selected={props.location.state.selected}
+            genres={props.location.state.genres}>
+          </FilmsPage>)}>
+      </Route>
 
-    <Route
-      path="/films"
-      render = { (props) => (
-        <FilmsPage
-          browseby={props.location.state.browseby}
-          selected={props.location.state.selected}
-          genres={props.location.state.genres}>
-        </FilmsPage>)}>
-    </Route>
+      <Route
+        path="/person"
+        render = { (props) => (
+          <PersonPage
+          credit={props.location.state.credit}>
+          </PersonPage>)}>
+      </Route>
+      
+      <Route
+        path="/user/"
+        component={ProfilePage}>
+      </Route>
 
-    <Route
-      path="/person"
-      render = { (props) => (
-        <PersonPage
-        credit={props.location.state.credit}>
-        </PersonPage>)}>
-    </Route>
-    
-    <Route
-      exact path="/:account"
-      component={ProfilePage}>
-    </Route>
+      <Route
+        path="/:account/watchlist"
+        component={UserFilmsPage}>
+      </Route>
 
-    <Route
-      path="/:account/watchlist"
-      component={UserFilmsPage}>
-    </Route>
+      <Route
+        path="/:account/ratings"
+        component={UserFilmsPage}>
+      </Route>
 
-    <Route
-      path="/:account/ratings"
-      component={UserFilmsPage}>
-    </Route>
+      <Route
+        path="/:account/favorites"
+        component={UserFilmsPage}>
+      </Route>
 
-    <Route
-      path="/:account/favorites"
-      component={UserFilmsPage}>
-    </Route>
+      <Route
+        path="/:account/lists"
+        component={UserListsPage}>
+      </Route>
 
-    <Route
-      path="/:account/lists"
-      component={UserListsPage}>
-    </Route>
-
-
-    
-
+      <Route
+        path="/404"
+        component={Error}>
+      </Route>
+      
+      <Redirect to="/404"></Redirect>
+      
+    </Switch>
   </Router>
+
 ); 
 
 export default Routes; 
---
+++ src/pages/Page404.js
@@ -0,0 +1,37 @@
+import React from "react";
+import styled from "styled-components";
+import Header from "../components/Header"; 
+import backdrop from "../the_new_world.jpg"; 
+
+function Page404() {
+  return (
+    <React.Fragment>
+      <Header></Header>
+      <Div>
+        <Message>404 Page Not Found</Message>
+        <div>Image from The New World (2005) | The Criterion Collection</div>
+      </Div>
+    </React.Fragment>
+  )
+}
+
+const Message = styled.div` 
+  height: 80vh;
+  font-size: 10rem; 
+  font-family: Roboto; 
+  text-align: center; 
+`;
+
+const Div = styled.div`
+  background: url(${backdrop}); 
+  background-repeat: no-repeat; 
+  margin-top: 2%;
+  background-position: center; 
+  color: white; 
+  text-align: center; 
+`; 
+
+
+
+
+export default Page404; 
